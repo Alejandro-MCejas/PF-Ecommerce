c26197f843235a8ee534ade49d56b6dfaff48ecc
AleCejas <alejandro.06555@gmail.com>
2024-11-06 18:46:10 -0300

    Cambiando configuracion typeorm y disponibilizando relacion en products

--- back/src/config/typeorm.ts
+++ back/src/config/typeorm.ts
@@ -2,16 +2,16 @@ import { registerAs } from "@nestjs/config";
 import { configDotenv } from "dotenv";
 import { DataSource, DataSourceOptions } from "typeorm";
 
-configDotenv({path:'.env.development'});
+configDotenv({path:'.env'});
 
 const config = {
     type: 'postgres',
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT as unknown as number,
+    port: Number(process.env.DB_PORT),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    entities:[__dirname + '/../**/*.entity{.ts,.js}'],
+    entities:['dist/**/*.entity{.ts,.js}'],
     migrations:['dist/migrations/*{.js,.ts}'],
     synchronize: true, // TRUE
     logging: false,
