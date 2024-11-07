3bd09c0d1220d160dcaf8c810a8d51158b6f4871
ValentinoSparvoli <Sparvolivalentino@gmail.com>
2024-11-07 20:22:36 -0300

    vista de todos los productos terminada y vista producto uno 60%

--- front/src/components/CardProductsView/CardProductsView.tsx
+++ front/src/components/CardProductsView/CardProductsView.tsx
@@ -1,9 +1,12 @@
 import { IProduct } from "@/interfaces/IProduct"
+import Link from "next/link"
 
 export const CardProductsView = ({product} : {product:IProduct})=>{
     return(
-        <div className="max-w-[200px] max-h-[270]  flex justify-center items-center my-3 hover:scale-[1.05]">
-            <img src={product.imagenBannerUrl} alt="" className="w-full h-full rounded-xl"/>
+        <div key={product.id} className="max-w-[200px] max-h-[270]  flex justify-center items-center my-3 hover:scale-[1.05]">
+            <Link href={`/products/${product.id}`}>
+                <img src={product.imagenBannerUrl} alt="" className="w-full h-full rounded-xl"/>
+            </Link>
         </div>
     )
 }
