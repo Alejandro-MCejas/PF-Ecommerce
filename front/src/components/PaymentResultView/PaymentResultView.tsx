9b69f70b20b03f957e34a5db0c97fe90f5f0f369
ValentinoSparvoli <Sparvolivalentino@gmail.com>
2024-11-19 17:03:53 -0300

    deploy1

--- front/src/components/PaymentResultView/PaymentResultView.tsx
+++ front/src/components/PaymentResultView/PaymentResultView.tsx
@@ -2,6 +2,9 @@
 
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
+import { getOrderDetailById } from '@/helpers/orderHelper';
+import { useAuth } from '@/context/Authcontext';
+import OrderDetailInformation from '../OrderDetailInformation/OrderDetailInformation';
 
 interface FeedbackData {
   collectionId: string | null;
@@ -15,9 +18,11 @@ interface FeedbackData {
   siteId: string | null;
   processingMode: string | null;
   merchantAccountId: string | null;
+  orderDetailId: string | null;
 }
 
 const PaymentResultView: React.FC = () => {
+  const { userData } = useAuth()
   const searchParams = useSearchParams();
   const [feedbackData, setFeedbackData] = useState<FeedbackData>({
     collectionId: null,
@@ -31,32 +36,66 @@ const PaymentResultView: React.FC = () => {
     siteId: null,
     processingMode: null,
     merchantAccountId: null,
+    orderDetailId: null,
   });
 
   useEffect(() => {
-    setFeedbackData({
-      collectionId: searchParams.get('collection_id'),
-      collectionStatus: searchParams.get('collection_status'),
-      paymentId: searchParams.get('payment_id'),
-      status: searchParams.get('status'),
-      externalReference: searchParams.get('external_reference'),
-      paymentType: searchParams.get('payment_type'),
-      merchantOrderId: searchParams.get('merchant_order_id'),
-      preferenceId: searchParams.get('preference_id'),
-      siteId: searchParams.get('site_id'),
-      processingMode: searchParams.get('processing_mode'),
-      merchantAccountId: searchParams.get('merchant_account_id'),
-    });
-  }, [searchParams]);
+    if (typeof window !== 'undefined') {
+      // Captura la URL completa
+      const url = window.location.href;
+
+      // Usa una expresión regular para capturar el `orderDetailId`
+      const orderDetailIdMatch = url.match(/orderDetailId:([^/?]+)/);
+      const orderDetailId = orderDetailIdMatch ? orderDetailIdMatch[1] : null;
+
+      // Obtiene los parámetros de la consulta
+      const queryParams = new URLSearchParams(window.location.search);
+
+      setFeedbackData({
+        orderDetailId,
+        collectionId: queryParams.get('collection_id'),
+        collectionStatus: queryParams.get('collection_status'),
+        paymentId: queryParams.get('payment_id'),
+        status: queryParams.get('status'),
+        externalReference: queryParams.get('external_reference'),
+        paymentType: queryParams.get('payment_type'),
+        merchantOrderId: queryParams.get('merchant_order_id'),
+        preferenceId: queryParams.get('preference_id'),
+        siteId: queryParams.get('site_id'),
+        processingMode: queryParams.get('processing_mode'),
+        merchantAccountId: queryParams.get('merchant_account_id'),
+      });
+
+      if (orderDetailId && userData?.token) {
+        getOrderDetailById(orderDetailId, userData.token);
+
+      }
+    }
+  }, []);
 
   return (
     <div>
-      <h1>Payment Feedback</h1>
-      {Object.entries(feedbackData).map(([key, value]) => (
-        <p key={key}>
-          {key}: {value || 'N/A'}
-        </p>
-      ))}
+      <div className='w-full flex justify-evenly items-center gap-2'>
+        <div>
+          {/* <p>Order detail ID:</p> */}
+          <p className='text-[30px] font-semibold'>Status:</p>
+          <p className='text-[30px] font-semibold'>ID:</p>
+        </div>
+        <div>
+          {/* <p>{feedbackData.orderDetailId}</p> */}
+          {
+            feedbackData.status === "approved" ?(
+              <p className='text-[30px] font-semibold text-green-500'>Approved</p>
+            ):(
+              <p className='text-[30px] font-semibold'>{feedbackData.status}</p>
+            )
+          }
+          <p className="text-[30px] font-semibold">{feedbackData.paymentId}</p>
+        </div>
+      </div>
+        <div className='w-full flex justify-center items-center my-3'>
+          <button className='bg-violet-500 w-[200px] h-[50px] rounded-lg text-white'>See my order detail</button>
+        </div>
     </div>
   );
 };
--- front/src/helpers/orderHelper.ts
+++ front/src/helpers/orderHelper.ts
@@ -1,4 +1,4 @@
-import { OrderData, OrderResponse } from "@/interfaces/IOrder";
+import { OrderData, OrderDetailInformation, OrderResponse } from "@/interfaces/IOrder";
 import { IProduct } from "@/interfaces/IProduct";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
@@ -6,57 +6,78 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 
 export const createOrder = async (orderData: OrderData): Promise<OrderResponse | null> => {
-    try {
-        const response = await fetch(`${API_URL}/orders`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${orderData.token}`, // si es necesario
-            },
-            body: JSON.stringify({
-                userId: orderData.userId,
-                products: orderData.products,
-            }),
-        });
+  try {
+    const response = await fetch(`${API_URL}/orders`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${orderData.token}`, // si es necesario
+      },
+      body: JSON.stringify({
+        userId: orderData.userId,
+        products: orderData.products,
+      }),
+    });
 
-        if (!response.ok) {
-            throw new Error(`Error en la solicitud: ${response.status} ${response.statusText}`);
-        }
-
-        const order = await response.json();
-        return order;
-    } catch (error) {
-        console.error("Error creando la orden:", error);
-        return null; // Retorna null o un valor de fallback en caso de error
+    if (!response.ok) {
+      throw new Error(`Error en la solicitud: ${response.status} ${response.statusText}`);
     }
+
+    const order = await response.json();
+    return order;
+  } catch (error) {
+    console.error("Error creando la orden:", error);
+    return null; // Retorna null o un valor de fallback en caso de error
+  }
 };
 
 interface PreferenceResponse {
   id: string;
 }
 
-export const createPaymentMercadoPago = async (orderId:string) => {
+export const createPaymentMercadoPago = async (orderId: string) => {
   try {
-    const response =  await fetch(`${API_URL}/mercado-pago/create-pago/${orderId}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Authorization': `Bearer ${orderData.token}`, // si es necesario
-        },
-        // body: JSON.stringify({
-        //     userId: orderData.userId,
-        //     products: orderData.products,
-        // }),
+    const response = await fetch(`${API_URL}/mercado-pago/create-pago/${orderId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Authorization': `Bearer ${orderData.token}`, // si es necesario
+      },
+      // body: JSON.stringify({
+      //     userId: orderData.userId,
+      //     products: orderData.products,
+      // }),
     })
 
     if (!response.ok) {
       throw new Error(`Error en la solicitud: ${response.status} ${response.statusText}`);
     }
     console.log(response)
-    
+
     const preference: PreferenceResponse = await response.json();
     return preference;
   } catch (error) {
     console.error('Error al crear la preferencia:', error);
   }
 };
+
+export const getOrderDetailById = async (orderId: string, token: string): Promise<OrderDetailInformation | null> => {
+  try {
+    const response = await fetch(`${API_URL}/products/${orderId}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error fetching order detail: ${response.statusText}`);
+    }
+
+    const orderDetail: OrderDetailInformation = await response.json();
+    return orderDetail;
+  } catch (error) {
+    console.error('Error fetching order detail:', error);
+    return null; // Asegura que la función siempre devuelva un valor
+  }
+};
+
